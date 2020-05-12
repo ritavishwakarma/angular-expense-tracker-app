0ac780640e872e1df2c1b6e2409a7a22c1354b6f
Rita <rita.17.rv@gmail.com>
2020-05-12 13:09:30 +0530

    localstorage

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ExpenseItem } from 'src/shared/models/expense-item.model';
+import { getInterpolationArgsLength } from '@angular/compiler/src/render3/view/util';
 
 @Component({
   selector: 'app-main-page',
@@ -12,12 +13,40 @@ export class MainPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    
+    this.getExpenseItems();  
+    this.getTotal();  
   }
-  addItem(newItem: ExpenseItem){
+  
+    getExpenseItems(){
+      if(localStorage.getItem('expenseItems')===null){
+          this.expenseItems =[];
+        }
+      else{
+          this.expenseItems = JSON.parse(localStorage.getItem('expenseItems')
+          );
+        }
+      }
+      getTotal(){
+        if(localStorage.getItem('totalAmount')===null){
+            this.totalAmount;
+          }
+        else{
+            this.totalAmount = JSON.parse(localStorage.getItem('totalAmount')
+            );
+          }
+        }
+  
+    
+    addItem(newItem: ExpenseItem){
     this.expenseItems.push(newItem);
     this.totalAmount += newItem.amount;
-
+    localStorage.setItem(
+      'expenseItems',JSON.stringify(this.expenseItems)
+    );
+    localStorage.setItem(
+      'totalAmount',JSON.stringify(this.totalAmount)
+    );
+  
   }
 
 }
